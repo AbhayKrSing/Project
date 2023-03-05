f1d33d41c47ec20689d5f658519498e563d93a75
Abhay <kabhay849@gmail.com>
2023-03-06 00:31:33 +0530

    pdf merger in node js done,now move to implement it on react

--- pdfmerger/src/components/Form.js
+++ pdfmerger/src/components/Form.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Form = () => {
+    const handlesubmmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className="form">
             <div className='w-25 mx-auto'>
-                <form className="mb-3" action='/merge' method='post'>
+                <form className="mb-3" action='/merge' method='post' onSubmit={handlesubmmit} encType='multipart/form-data'>
                     <label htmlFor="formFile" className="form-label"></label>
                     <input className="form-control" type="file" multiple id="formFile" name='files' />
-                    <button type='submit' class="btn btn-primary">Submit</button>
+                    <button type='submit' className="btn btn-primary" value='upload' >Merge</button>
                 </form>
                 {/* <form enctype='multipart/form-data' method='POST' action='/merge'>
                 <input type='file' name='files' multiple />
