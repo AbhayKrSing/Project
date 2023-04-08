02c6ba7c2bebae4d7c3238756cc00d5ec4282400
Abhay <kabhay849@gmail.com>
2023-04-08 21:58:43 +0530

    Resume template added in one slider

--- Resume Builder/src/components/Authorized.js
+++ Resume Builder/src/components/Authorized.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import Resume from './Resume'
 
 const Authorized = () => {
-  return (
+  return (<>
     <div className='auth'>
-          
+      <h1 className='text-center'>Select template</h1>
+
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <Resume/>      
+          </div>
+          <div className="carousel-item">
+            <img src="https://th.bing.com/th/id/R.14692498181e6bdfe489d518f35b5e10?rik=IznoVeXPefr6WQ&riu=http%3a%2f%2fi.ytimg.com%2fvi%2fZADf8p5-c2s%2f0.jpg&ehk=Nwg8NjOZvCbpc%2bQpe6fFKkQM5FxuZiPH0BOYLBMKFB4%3d&risl=&pid=ImgRaw&r=0" className="d-block slider-img mx-auto" alt="img" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://th.bing.com/th/id/OIP.TA-__fSyGOWGEo_Qc_tYEAHaEU?pid=ImgDet&w=1024&h=597&rs=1" className="d-block slider-img mx-auto" alt="img" />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
+
+  </>
   )
 }
 
---
+++ Resume Builder/src/components/Resume.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className="resume-1 container-fluid bg-success mt-5">
+      <div className="row">
+        <div className="col-lg-8 mx-auto">
+          <h1 className="text-center mb-5">John Doe</h1>
+          <h2 className="mb-4">Personal Information</h2>
+          <p><strong>Email:</strong> john.doe@email.com</p>
+          <p><strong>Phone:</strong> (123) 456-7890</p>
+          <p><strong>Address:</strong> 123 Main Street, Anytown USA</p>
+          <h2 className="mb-4 mt-5">Education</h2>
+          <ul>
+            <li><strong>Bachelor of Science in Computer Science</strong>, XYZ University, Anytown USA</li>
+            <li><strong>Master of Science in Information Technology</strong>, ABC University, Anytown USA</li>
+          </ul>
+          <h2 className="mb-4 mt-5">Work Experience</h2>
+          <div className="media">
+            <img src="https://via.placeholder.com/64" className="align-self-start mr-3" alt="..." />
+            <div className="media-body">
+              <h5 className="mt-0">Software Engineer, XYZ Company</h5>
+              <p>Developed and maintained software applications using Java and Python.</p>
+              <p>Collaborated with cross-functional teams to deliver high-quality software products.</p>
+            </div>
+          </div>
+          <div className="media mt-4">
+            <img src="https://via.placeholder.com/64" className="align-self-start mr-3" alt="..." />
+            <div className="media-body">
+              <h5 className="mt-0">Systems Analyst, ABC Corporation</h5>
+              <p>Designed and implemented computer systems for various clients.</p>
+              <p>Provided technical support and troubleshooting to clients.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Resume
