8a467bd5ecf7a8df78344b8147632c84c6f58d1f
Abhay <kabhay849@gmail.com>
2023-04-23 11:08:21 +0530

    login and signu page in progress

--- ChatApp/frontend/src/App.js
+++ ChatApp/frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from './Pages/Homepage'
 import Chatpage from './Pages/Chatpage';
 function App() {
   return (
 
-
+    <div className="App">
       <Routes>
         <Route >
-          <Route path="/" element={<Homepage/>} />
-          <Route path="/chats" element={<Chatpage/>} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/chats" element={<Chatpage />} />
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
+    </div>
   );
 }
 
--- ChatApp/frontend/src/Pages/Homepage.js
+++ ChatApp/frontend/src/Pages/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Container, Box, Text } from '@chakra-ui/react'
 const Homepage = () => {
   return (
     <React.Fragment>
-      I M Homepage
+      <Container p={4}>
+        <Box bg='white' w='100%' p={4} m={10} borderRadius={'lg'} textAlign={'center'} color='black'>
+          <Text fontSize={'4xl'} fontFamily={'work sans'}> Talk-tive</Text>
+        </Box>
+      </Container>
     </React.Fragment>
   )
 }
