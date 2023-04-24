890afec08bc44ab3eaf2a5d83c1536cb8b1cebb2
Abhay <kabhay849@gmail.com>
2023-04-24 14:31:28 +0530

    mongodb atlas successfully connected using .env file

--- ChatApp/Backend/Models/UserModel.js
+++ ChatApp/Backend/Models/UserModel.js
@@ -10,6 +10,7 @@ const UserSchema = new mongoose({
         type: String,
         required: true,
         min: 10,
+        unique: true
     },
     password: {
         type: String,
@@ -18,7 +19,6 @@ const UserSchema = new mongoose({
     },
     pic: {
         type: String,
-        required: true,
         default: 'https://th.bing.com/th/id/R.6b0022312d41080436c52da571d5c697?rik=Ql6UUNosrWAY0w&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fpng-user-icon-icons-logos-emojis-users-2400.png&ehk=8agkVrs8bo9zafVF9Qk4%2bFqv5IwaEZrb8DwX%2ftfJtNc%3d&risl=&pid=ImgRaw&r=0'
     }
 
--- ChatApp/Backend/server.js
+++ ChatApp/Backend/server.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const path = require('path');
+const colors = require('colors')
+require('dotenv').config({ path: path.resolve(__dirname, './.env') }); // According to document,Best pratice must be like this.
 const app = express()
-require('dotenv').config()
-app.get('/',  (req, res)=> {
-  res.send('API is Running')
+const connectDB = require('./config/connectdb')
+connectDB()
+app.get('/', (req, res) => {
+    res.send('API is Running')
 })
-app.get('/api/chats',(req,res)=>{
+app.get('/api/chats', (req, res) => {
     res.send('You will see all chats')
 })
-app.get('/api/chats/:id',(req,res)=>{
+app.get('/api/chats/:id', (req, res) => {
     res.send('You will see User specific chats')
 })
 
 
 
-app.listen(process.env.SERVER_HOST,()=>{
-    console.log('Server is running on 5000')
+app.listen(process.env.SERVER_HOST, () => {
+    console.log('Server is running on 5000'.yellow.bold)
 })
