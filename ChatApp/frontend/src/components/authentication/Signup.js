82741d0d412900c413ee9a723bd571263f835530
Abhay <kabhay849@gmail.com>
2023-04-27 00:43:06 +0530

    img file store on cloudnary and its url saved in database with user's other datas.

--- ChatApp/frontend/src/components/authentication/Signup.js
+++ ChatApp/frontend/src/components/authentication/Signup.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 import { Input, VStack, FormControl, FormLabel, FormHelperText, Button } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
-
+import { useNavigate } from 'react-router-dom'
+import axios from "axios";
 const Signup = () => {
+    const navigate = useNavigate()
     const toast = useToast()
     const [loading, setloading] = useState(false)
-    const [credentials, setcredentials] = useState({ name: '', email: '', password: '', cpassword: '', img: '' })
+    const [credentials, setcredentials] = useState({ name: '', email: '', password: '', cpassword: '', pic: '' })
     const handlechange = (e) => {
         setcredentials({ ...credentials, [e.target.name]: e.target.value })
     }
     const processPics = (pic) => {
+        const { name, email, password, cpassword } = credentials
         setloading(true)
         if (!pic) {
             toast({
                 title: 'Please select image',
                 description: "Account creation Failed due to internal server error",
-                status: 'danger',
+                status: 'warning',
                 duration: 5000,
                 isClosable: true,
             })
@@ -35,7 +38,7 @@ const Signup = () => {
                 .then(response => response.json())
                 .then((data) => {
                     console.log(data)
-                    setcredentials({ img: data.url.toString() })  //img url in cloudnary
+                    setcredentials({ name: name, email: email, password: password, cpassword: cpassword, pic: data.url.toString() })  //img url in cloudnary
                     setloading(false)
                 })
 
@@ -47,31 +50,76 @@ const Signup = () => {
 
     }
     const handleSubmit = () => {
+        const { name, email, password, cpassword, pic } = credentials
+        // console.log(name, email, password, cpassword, pic)
+
+        if (!name || !email || !password) {
+            setloading(true)
+            toast({
+                title: 'Please fill all fields',
+                description: "Account creation Failed",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+            })
+            setloading(false)
+            return
+        }
+        else if (!(password === cpassword)) {
+            setloading(true)
+            toast({
+                title: 'Password must matches to confirm Password',
+                description: "Account creation Failed",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+            })
+            setloading(false)
+            return
+        }
+        else {
+            setloading(true)
+            axios.post("api/user", {
+                name: name,
+                email: email,
+                password: password,
+                pic: pic
+            }).then((response) => {
+                const { data } = response
+                localStorage.setItem('UserInfo', data.token)
+                setloading(false)
+                navigate('/chats')
+
+            }).catch((err) => {
+                console.log(err.message)
+                setloading(false)
+            })
+        }
 
     }
     return (
         <React.Fragment>
 
             <VStack spacing={'10'}>
-                <FormControl isRequired>
+                <FormControl >
                     <FormLabel>Name</FormLabel>
-                    <Input type='text' marginBottom={4} id='name' name='name' onChange={handlechange} />
+                    <Input type='text' marginBottom={4} id='name' name='name' onChange={handlechange} isRequired />
 
                     <FormLabel>Email address</FormLabel>
-                    <Input type='email' id='email' name='email' onChange={handlechange} />
+                    <Input type='email' id='email' name='email' onChange={handlechange} isRequired />
                     <FormHelperText textAlign={'start'} marginBottom={4}>We'll never share your email.</FormHelperText>
 
                     <FormLabel>Password</FormLabel>
-                    <Input type='password' id='password' name='password' onChange={handlechange} />
+                    <Input type='password' id='password' name='password' onChange={handlechange} isRequired />
                     <FormHelperText textAlign={'start'} marginBottom={4}>Must be strong password</FormHelperText>
 
                     <FormLabel>Confirm Password</FormLabel>
-                    <Input type='password' marginBottom={4} id='cpassword' name='cpassword' onChange={handlechange} />
+                    <Input type='password' marginBottom={4} id='cpassword' name='cpassword' onChange={handlechange} isRequired />
 
                     <FormLabel>Upload Img</FormLabel>
                     <Input type="file" id="img" accept="image/*" marginBottom={4} onChange={(e) => { processPics(e.target.files[0]) }} />
 
-                    <Button id='submit' cursor={'pointer'} width={'100%'} background={'blue.300'} onClick={handleSubmit} isLoading={loading}>SignUp</Button>
+                    <Button id='submit' disabled={false} cursor={'pointer'} width={'100%'} background={'blue.300'} isLoading={loading} onClick={handleSubmit}>SignUp</Button>
                 </FormControl>
             </VStack>
 
