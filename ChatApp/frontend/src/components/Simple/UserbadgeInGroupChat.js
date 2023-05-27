bdcf96ff15ba25eb3d57b86af643eb4cc034fa09
Abhay <kabhay849@gmail.com>
2023-05-27 18:45:37 +0530

    pointer-event:'none' added to non admin user in badges

--- ChatApp/frontend/src/components/Simple/UserbadgeInGroupChat.js
+++ ChatApp/frontend/src/components/Simple/UserbadgeInGroupChat.js
@@ -3,14 +3,14 @@ import { UseContextAPI } from '../../Context/ChatProvider'
 import { Stack, Badge } from '@chakra-ui/react'
 import { CloseIcon } from '@chakra-ui/icons'
 const UserbadgeInGroupChat = () => {
-    const { People, remove } = UseContextAPI()
+    const { People, remove, selectChat, user } = UseContextAPI()
     return (
 
         <Stack direction='row' wrap={'wrap'}>
             {(People).map((element) => {
                 return (
                     <span style={{ margin: '2px' }} key={element._id}><Badge _disabled={true} colorScheme='purple' bg={'peru'} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>{element.name}
-                        <CloseIcon onClick={() => { remove(element) }} />
+                        <CloseIcon onClick={() => { remove(element) }} pointerEvents={user.id === selectChat.groupAdmin._id ? "" : 'none'} />
                     </Badge>
                     </span>)
             })}
