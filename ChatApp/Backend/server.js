1eec100744b93e709870f68484d2e4c20ca1f3fc
Abhay <kabhay849@gmail.com>
2024-06-08 14:50:26 +0530

    done with server

--- ChatApp/Backend/server.js
+++ ChatApp/Backend/server.js
@@ -28,7 +28,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: ['http://localhost:3000']
+        origin: ['https://talktive.netlify.app']
     },
 })
 
